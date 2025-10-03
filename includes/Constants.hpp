fe2ef56c4535e3485dd34e095fe3125e45ebcc61
Hamad <AlmarzooqiH23@gmail.com>
2025-10-03 19:42:47 +0400

    Added constants headerfile to manage constants.

--- includes/Constants.hpp
+++ includes/Constants.hpp
@@ -1,19 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   constants.hpp                                      :+:      :+:    :+:   */
+/*   Constants.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hamalmar <hamalmar@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 16:38:26 by hamalmar          #+#    #+#             */
-/*   Updated: 2025/10/01 16:51:00 by hamalmar         ###   ########.fr       */
+/*   Updated: 2025/10/03 19:35:43 by hamalmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONSTANTS_HPP
 # define CONSTANTS_HPP
-
-	//For the LULS
-	const int PORT = 6942;
+	#include "SocketHeaders.hpp"
+	const std::string SERVER_IP = std::string("127.0.0.1");
+	const unsigned int NUMBER_OF_CLIENTS = SOMAXCONN;
+	const unsigned int MS_TIMEOUT = 30000;
+	const int RESERVED_PORTS = IPPORT_RESERVED;
+	const int MAX_PORTS = 65535;
 
 #endif
