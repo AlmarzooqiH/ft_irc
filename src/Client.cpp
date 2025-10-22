baa8c87295306983ccff448da5388c1917ed15d4
Hamad <AlmarzooqiH23@gmail.com>
2025-10-22 16:31:35 +0400

    Initalized the new members and added their getters and setters.

--- src/Client.cpp
+++ src/Client.cpp
@@ -15,7 +15,10 @@
 Client::Client() :
 username(""),
 nickname(""),
-role("")
+role(""),
+recvNick(false),
+recvUser(false),
+recvCab(false)
 
 {}
 
@@ -24,7 +27,10 @@ Client::~Client(){}
 Client::Client(const Client& right) :
 username(right.username),
 nickname(right.nickname),
-role(right.role)
+role(right.role),
+recvNick(right.recvNick),
+recvUser(right.recvUser),
+recvCab(right.recvCab)
 {}
 
 Client& Client::operator=(const Client& right){
@@ -32,6 +38,9 @@ Client& Client::operator=(const Client& right){
 		this->username = right.username;
 		this->nickname = right.nickname;
 		this->role = right.role;
+		this->recvNick = right.recvNick;
+		this->recvUser = right.recvUser;
+		this->recvCab = right.recvCab;
 	}
 	return (*this);
 }
@@ -39,7 +48,13 @@ Client& Client::operator=(const Client& right){
 std::string	Client::getNickname(void) const {return (this->nickname);}
 std::string	Client::getUsername(void) const {return (this->username);}
 std::string	Client::getRole(void) const {return (this->role);}
+bool	Client::getRecvNick(void) const {return (this->recvNick);}
+bool	Client::getRecvUser(void) const {return (this->recvUser);}
+bool	Client::getRecvCab(void) const {return (this->recvCab);}
 
 void	Client::setNickname(std::string &nNickname) {this->nickname = nNickname;}
 void	Client::setUsername(std::string &nUsername) {this->username = nUsername;}
 void	Client::setRole(std::string &nRole) {this->role = nRole;}
+void	Client::setRecvNick(bool n) {this->recvNick = n;}
+void	Client::setRecvUser(bool n) {this->recvUser = n;}
+void	Client::setRecvCab(bool n) {this->recvCab = n;}
